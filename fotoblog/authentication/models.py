1d59ac9b5689c15949af5356062bd66829f44d67
sangmin <tkdals4723@gmail.com>
2022-05-20 11:09:22 +0200

    create models.py and migrations success

--- fotoblog/authentication/models.py
+++ fotoblog/authentication/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from authentication.constants import RoleType
 
-# Create your models here.
+
+class User(AbstractUser):
+
+    profil_photo = models.ImageField()
+    role = models.CharField(
+        max_length=30, 
+        choices=RoleType.choices, 
+        default=RoleType.CREATOR,
+        verbose_name="Rôle")
